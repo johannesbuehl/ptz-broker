fc1e1da0738a3c846ac964e9d9ed39122bd28465
Sophie <sophie.nagel@famna.de>
2025-12-14 22:11:31 +0100

    Stand 14.12.

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -20,7 +20,7 @@ type Config struct {
 	Camera  struct {
 		Adress       Adress `json:"adress" validate:"required"`
 		Speed        byte   `json:"speed" validate:"required"`
-		WhiteBalance string `json:"whitebalance" validate:"required"`
+		WhiteBalance []byte `json:"whitebalance" validate:"required"`
 	} `json:"camera" validate:"required"`
 	OSCPort uint `json:"osc_port" validate:"required,port"`
 
--- pkg/positionPreset/positionPreset.go
+++ pkg/positionPreset/positionPreset.go
@@ -1,16 +1,14 @@
 package positionPreset
 
 import (
-	"bufio"
 	"net"
-	"strings"
 	"sync"
 )
 
 type Position struct {
-	Pan  string `json:"pan"`
-	Tilt string `json:"tilt"`
-	Zoom string `json:"zoom"`
+	Pan  [4]byte `json:"pan"`
+	Tilt [4]byte `json:"tilt"`
+	Zoom [4]byte `json:"zoom"`
 }
 
 type positionMutex struct {
@@ -21,7 +19,8 @@ type positionMutex struct {
 
 func (p Position) RecallCameraPosition(connection *net.TCPConn) error {
 	mu := positionMutex{
-		ch: make(chan error),
+		ch:       make(chan error),
+		Position: p,
 	}
 
 	go mu.Position.recallPanTilt(connection)
@@ -40,18 +39,22 @@ func (p Position) RecallCameraPosition(connection *net.TCPConn) error {
 }
 
 func (p Position) recallPanTilt(connection *net.TCPConn) error {
-	command := "81 01 06 02 18 14 " + strings.Join([]string{p.Pan, p.Tilt}, " ") + " FF"
+	command := []byte{
+		0x81, 0x01, 0x06, 0x02, 0x18, 0x14, p.Pan[0], p.Pan[1], p.Pan[2], p.Pan[3], p.Tilt[0], p.Tilt[1], p.Tilt[2], p.Tilt[3], 0xFF,
+	}
 
-	if _, err := connection.Write([]byte(command)); err != nil {
+	if _, err := connection.Write(command); err != nil {
 		return err
 	} else {
 		return nil
 	}
 }
 func (z Position) recallZoom(connection *net.TCPConn) error {
-	command := "81 01 04 47 " + z.Zoom + " FF"
+	command := []byte{
+		0x81, 0x01, 0x04, 0x47, z.Zoom[0], z.Zoom[1], z.Zoom[2], z.Zoom[3], 0xFF,
+	}
 
-	if _, err := connection.Write([]byte(command)); err != nil {
+	if _, err := connection.Write(command); err != nil {
 		return err
 	} else {
 		return nil
@@ -78,25 +81,7 @@ func GetCameraPosition(connection *net.TCPConn) (Position, error) {
 }
 
 func (c *positionMutex) getPanTilt(connection *net.TCPConn) {
-	if _, err := connection.Write([]byte("81 09 06 12 FF\n")); err != nil {
-		c.ch <- err
-	} else {
-		reader := bufio.NewReader(connection)
-
-		if line, err := reader.ReadString('\n'); err != nil {
-			c.ch <- err
-		} else {
-			c.mu.Lock()
-			c.Position.Pan = line[6:17]
-			c.Position.Tilt = line[18:29]
-			c.mu.Unlock()
-			c.ch <- nil
-		}
-	}
-}
-
-func (c *positionMutex) getZoom(connection *net.TCPConn) {
-	if _, err := connection.Write([]byte("81 09 04 47 FF")); err != nil {
+	if _, err := connection.Write([]byte{0x81, 0x09, 0x06, 0x12, 0xFF}); err != nil {
 		c.ch <- err
 	} else {
 		response := make([]byte, 32)
@@ -105,7 +90,36 @@ func (c *positionMutex) getZoom(connection *net.TCPConn) {
 			c.ch <- err
 		} else {
 			c.mu.Lock()
-			c.Position.Zoom = string(response)[6:17]
+			// c.Position.Pan = line[6:17]
+			// c.Position.Tilt = line[18:29]
+			c.Position.Pan[0] = response[2]
+			c.Position.Pan[1] = response[3]
+			c.Position.Pan[2] = response[4]
+			c.Position.Pan[3] = response[5]
+			c.Position.Tilt[0] = response[6]
+			c.Position.Tilt[1] = response[7]
+			c.Position.Tilt[2] = response[8]
+			c.Position.Tilt[3] = response[9]
+			c.mu.Unlock()
+			c.ch <- nil
+		}
+	}
+}
+
+func (c *positionMutex) getZoom(connection *net.TCPConn) {
+	if _, err := connection.Write([]byte{0x81, 0x09, 0x04, 0x47, 0xFF}); err != nil {
+		c.ch <- err
+	} else {
+		response := make([]byte, 7)
+
+		if _, err := connection.Read(response); err != nil {
+			c.ch <- err
+		} else {
+			c.mu.Lock()
+			c.Position.Zoom[0] = response[2]
+			c.Position.Zoom[1] = response[3]
+			c.Position.Zoom[2] = response[4]
+			c.Position.Zoom[3] = response[5]
 			c.mu.Unlock()
 
 			c.ch <- nil
