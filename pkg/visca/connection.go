98cb253eec1c050e91c1bb1d6cf9e8ffad6d173f
z1glr <z1glr@pm.me>
2025-12-21 15:45:54 +0100

    visca communication should work now

--- pkg/visca/connection.go
+++ pkg/visca/connection.go
@@ -1,16 +1,21 @@
 package visca
 
 import (
-	"errors"
+	"bufio"
 	"fmt"
-	"io"
+	"math"
 	"net"
+	"sync"
+	"time"
 )
 
 type Camera struct {
-	IP         string
-	Port       uint16
-	connection *net.TCPConn
+	IP             string
+	Port           uint16
+	connection     *net.TCPConn
+	mu             sync.Mutex
+	channels       map[viscaSocket]chan []byte
+	defaultChannel chan []byte
 }
 
 func (c *Camera) Connect() error {
@@ -21,6 +26,29 @@ func (c *Camera) Connect() error {
 	} else if err := c.connection.SetKeepAlive(true); err != nil {
 		return err
 	} else {
+		// initialize sockets
+		c.defaultChannel = make(chan []byte)
+		c.channels = make(map[viscaSocket]chan []byte)
+
+		for socket := range viscaSocket(8) {
+			c.channels[socket] = make(chan []byte)
+		}
+
+		go func() {
+			reader := bufio.NewReader(c.connection)
+
+			for {
+				if content, err := reader.ReadBytes(0xFF); err == nil {
+					// check for a resonse with a socket (that is not an ACK)
+					if response, socket := parseViscaResponse(content); response == RESP_COMPLETION || response == RESP_COMMAND_CANCELLED || response == RESP_NO_SOCKET || response == RESP_COMMAND_NOT_EXECUTABLE {
+						c.channels[socket] <- content
+					} else {
+						c.defaultChannel <- content
+					}
+				}
+			}
+		}()
+
 		return nil
 	}
 }
@@ -31,31 +59,95 @@ func (c *Camera) Disconnect() error {
 
 const BUFFER_LEN = 32
 
-func (c *Camera) sendViscaBytes(b []byte) ([]byte, error) {
+type (
+	viscaResponse uint8
+	viscaSocket   byte
+)
+
+const (
+	RESP_ACK viscaResponse = iota
+	RESP_COMPLETION
+	RESP_SYNTAX_ERROR
+	RESP_COMMAND_BUFFER_FULL
+	RESP_COMMAND_CANCELLED
+	RESP_NO_SOCKET
+	RESP_COMMAND_NOT_EXECUTABLE
+	RESP_INQUIRY
+)
+
+func parseViscaResponse(msg []byte) (viscaResponse, viscaSocket) {
+	var response viscaResponse
+	socket := viscaSocket(msg[1] % 16)
+
+	switch msg[1] >> 4 {
+	case 4:
+		response = RESP_ACK
+	case 5:
+		if len(msg) == 3 {
+			response = RESP_COMPLETION
+		} else {
+			response = RESP_INQUIRY
+		}
+	case 6:
+		switch msg[2] {
+		case 2:
+			socket = math.MaxUint8
+			response = RESP_SYNTAX_ERROR
+		case 3:
+			socket = math.MaxUint8
+			response = RESP_COMMAND_BUFFER_FULL
+		case 4:
+			response = RESP_COMMAND_CANCELLED
+		case 5:
+			response = RESP_NO_SOCKET
+		case 0x41:
+			response = RESP_COMMAND_NOT_EXECUTABLE
+		}
+	}
+
+	return response, socket
+}
+
+func getResponse(ch chan []byte) ([]byte, error) {
+	select {
+	case response := <-ch:
+		return response, nil
+	case <-time.After(10 * time.Second):
+		return nil, fmt.Errorf("timed out waiting for channel message")
+	}
+}
+
+func (c *Camera) sendCommand(b []byte) (chan []byte, error) {
+	c.mu.Lock()
+
 	if _, err := c.connection.Write(b); err != nil {
+		c.mu.Unlock()
 		return nil, err
 	} else {
-		// read all data
-		responseBuffer := make([]byte, BUFFER_LEN)
-		response := []byte{}
+		response, err := getResponse(c.defaultChannel)
+		c.mu.Unlock()
 
-		for {
-			if readBytes, err := c.connection.Read(responseBuffer); err != nil {
-				if errors.Is(err, io.EOF) {
-					break
+		if err != nil {
+			return nil, err
+
+			// if it was an acknowledge, wait for the completion message
+		} else {
+			responseChannel := make(chan []byte)
+
+			go func() {
+				defer close(responseChannel)
+
+				if resp, socket := parseViscaResponse(response); resp == RESP_ACK {
+					if response, err := getResponse(c.channels[socket]); err != nil {
+					} else {
+						responseChannel <- response
+					}
 				} else {
-					return nil, err
+					responseChannel <- response
 				}
-			} else if readBytes < BUFFER_LEN {
-				response = append(response, responseBuffer[:readBytes]...)
-				break
-			} else {
-				response = append(response, responseBuffer...)
-			}
+			}()
+
+			return responseChannel, nil
 		}
-
-		// TODO: parse returned message for response message
-
-		return response, nil
 	}
 }
--- pkg/visca/panTiltZoom.go
+++ pkg/visca/panTiltZoom.go
@@ -10,15 +10,31 @@ func (c *Camera) PanTiltAbsolute(pt PanTilt) error {
 		0x81, 0x01, 0x06, 0x02, 0x18, 0x14, pt.Pan[0], pt.Pan[1], pt.Pan[2], pt.Pan[3], pt.Tilt[0], pt.Tilt[1], pt.Tilt[2], pt.Tilt[3], 0xFF,
 	}
 
-	_, err := c.sendViscaBytes(command)
+	if ch, err := c.sendCommand(command); err != nil {
+		return err
+	} else {
+		// wait for message
+		if msg, err := getResponse(ch); err != nil {
+			return err
+		} else {
+			// if the message is an acknowledge, wait for the completion
+			if resp, _ := parseViscaResponse(msg); resp == RESP_ACK {
+				if _, err := getResponse(ch); err != nil {
+					return err
+				}
+			}
 
-	return err
+			return nil
+		}
+	}
 }
 
 func (c *Camera) GetPanTilt() (PanTilt, error) {
-	if response, err := c.sendViscaBytes([]byte{0x81, 0x09, 0x06, 0x12, 0xFF}); err != nil {
+	if ch, err := c.sendCommand([]byte{0x81, 0x09, 0x06, 0x12, 0xFF}); err != nil {
 		return PanTilt{}, err
 	} else {
+		response := <-ch
+
 		return PanTilt{
 			Pan: [4]byte{
 				response[2],
@@ -32,6 +48,47 @@ func (c *Camera) GetPanTilt() (PanTilt, error) {
 				response[8],
 				response[9],
 			},
+		}, err
+	}
+}
+
+type Zoom [4]byte
+
+func (c *Camera) Zoom(zoom Zoom) error {
+	command := []byte{
+		0x81, 0x01, 0x04, 0x47, zoom[0], zoom[1], zoom[2], zoom[3], 0xFF,
+	}
+
+	if ch, err := c.sendCommand(command); err != nil {
+		return err
+	} else {
+		// wait for message
+		if msg, err := getResponse(ch); err != nil {
+			return err
+		} else {
+			// if the message is an acknowledge, wait for the completion
+			if resp, _ := parseViscaResponse(msg); resp == RESP_ACK {
+				if _, err := getResponse(ch); err != nil {
+					return err
+				}
+			}
+
+			return nil
+		}
+	}
+}
+
+func (c *Camera) GetZoom() (Zoom, error) {
+	if ch, err := c.sendCommand([]byte{0x81, 0x09, 0x06, 0x12, 0xFF}); err != nil {
+		return Zoom{}, err
+	} else {
+		response := <-ch
+
+		return Zoom{
+			response[2],
+			response[3],
+			response[4],
+			response[5],
 		}, err
 	}
 }
